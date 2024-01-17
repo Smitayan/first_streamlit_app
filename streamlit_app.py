138a256e2a17356a9a52c1417ede800a267202fa
Smitayan <54241756+Smitayan@users.noreply.github.com>
2024-01-17 20:37:25 +0530

    Update streamlit_app.py
    added select list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 file_path='https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 fruits_df = pd.read_csv(file_path)
+fruits_df = fruits_df.set_index('Fruit')
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu:")
 streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
@@ -9,4 +10,5 @@ streamlit.text("🥗 Kale,Spinach & Rocket smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑 🍞 Avocado Toast")
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+streamlit.multiselect("Pick some fruits:",list(fruits_df.index))
 streamlit.dataframe(fruits_df)
