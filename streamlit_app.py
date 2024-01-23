552d170523dbb1d0f04beb396c90e1b6c9fc0f29
Smitayan <54241756+Smitayan@users.noreply.github.com>
2024-01-23 23:16:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas as pd
+import requests
+
 file_path='https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 fruits_df = pd.read_csv(file_path)
 fruits_df = fruits_df.set_index('Fruit')
@@ -13,6 +15,8 @@ streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 fruits_selected=streamlit.multiselect("Pick some fruits:",list(fruits_df.index))
 fruits_selected_data=fruits_df.loc[fruits_selected]
 streamlit.dataframe(fruits_selected_data)
-import requests
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
